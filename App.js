6f824b2d84681ff8c7f9a0e46f368a6cb4845711
Luddmilita <ludmila.molina634@gmail.com>
2023-04-27 22:58:04 -0300

    init

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>CD/CI testing with Bitrise</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 20,
+    color: "white",
+    fontWeight: 900
+  }
 });
